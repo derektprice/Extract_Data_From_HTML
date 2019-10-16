785fd190669cb11f2f4fa17f15e65d2471c5b040
Derek P <derektprice@gmail.com>
2019-10-15 23:08:36 -0500

    Update Extract_Data_From_File_1.0.py
    This one finally works. Big changes:
    added error handling to "open" in line 26
    changed write_to_excel to accept arrays of different lengths by transposing the data in the frame
    I don't like the fact that my data goes out to the right in my csv files and the "column headings" disappear, but this is actually functional

--- Extract_Data_From_File_1.0.py
+++ Extract_Data_From_File_1.0.py
@@ -2,8 +2,7 @@ from bs4 import BeautifulSoup
 import soupsieve as sv
 import pandas as pd
 
-# put all html files you want to extract data from into "target_files"
-target_files = ["Github tutorial 0 - 08-October-2019--11.20.html"]
+target_files = ["GitHub Guide 0 - 15-October-2019--22.34.html"]
 column1 = []
 column2 = []
 column3 = []
@@ -11,34 +10,30 @@ column4 = []
 column5 = []
 column6 = []
 
-# extracts urls from a target css selector
+# give soupsieve target, name of list to output to, and soup
 def get_url(sv_target, targetlist, soup):
     key_tags1 = sv.select(sv_target, soup)
-    outputlist = []
     for link in key_tags1:
         targetlist.append(link.get("href"))
 
-# extracts strings from a target css selector
 def get_text(sv_target, targetlist, soup):
     key_tags2 = sv.select(sv_target, soup)
     for tag2 in key_tags2:
         targetlist.append(tag2.text)
 
-#writes data to differen columns in a pandas dataframe, which is converted then to a csv
-def write_to_excel(filename, col1name, col2name=None, col3name=None, col4name=None, col5name=None, col6name=None):
-    df1 = pd.DataFrame({col1name: column1, col2name: column2})
-    df1.to_csv(filename, mode="a", index=False, encoding="utf-8-sig")
-        
-#runs "gets" on each file in target_files, saves to a csv with write_to_excel
 def get_all_data():
     for thing in target_files:
-        openedfile = open(thing)
+        openedfile = open(thing, errors="surrogateescape") #this error parameter made my programs work together
         soup = BeautifulSoup(openedfile, "html.parser")
-        #insert "get_text"(s) and "get_url"(s) below, they run on every html file in list "target_files"
-        #example: get_text("title
-        get_text("td.titleColumn > a", column1, soup)
-        get_text("td.ratingColumn > strong", column2, soup)
+        get_text("div > h4", column1, soup)
+        get_text("div.wrap > h1", column2, soup)
+        #additional things you want in additional columns
         openedfile.close()
-    write_to_excel("test.csv", "Titles", "Ratings")
+    write_to_excel("test.csv", "Bolded Headers", "Hello World")
+
+def write_to_excel(filename, col1name, col2name, col3name=None, col4name=None, col5name=None, col6name=None):
+    resultsdict = {"Headers" : column1, "Hello World" : column2}
+    df1 = pd.DataFrame.from_dict(resultsdict, orient="index")
+    df1.to_csv(filename, mode="a", index=False, encoding="utf-8-sig")
 
 get_all_data()
